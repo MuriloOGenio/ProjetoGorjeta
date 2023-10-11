34122bf0fff1aa8e0d7484f854dd5169a7c736bc
MuriloOGenio <mueugi@gmail.com>
2023-10-10 21:51:03 -0300

    Colocando um pouco mais de java

--- app/src/main/java/br/com/etecia/myapp/CalculoActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CalculoActivity.java
@@ -3,21 +3,43 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.EventListener;
 
 public class CalculoActivity extends AppCompatActivity {
 
-    TextInputLayout txtValor = findViewById(R.id.txtValor);
-    TextInputLayout txtQualidade = findViewById(R.id.txtQualidade);
-    Button btnCalcular = findViewById(R.id.btnCalcular);
+    TextInputEditText txtValor;
+    TextInputEditText txtQualidade;
+    Button btnCalcular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        txtQualidade = findViewById(R.id.txtQualidade);
+        txtValor = findViewById(R.id.txtValor);
+
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int valor, qualidade;
+                valor = Integer.parseInt(txtValor.getText().toString());
+                qualidade = Integer.parseInt(txtQualidade.getText().toString());
+
+                Gorjeta gorjeta = new Gorjeta();
+                gorjeta.setValor(valor);
+                gorjeta.setQualidade(qualidade);
+            }
+        });
     }
 
 
