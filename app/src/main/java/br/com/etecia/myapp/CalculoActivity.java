b68bffb58fd12f7e48a5d031af518f3d867f5e46
MuriloOGenio <mueugi@gmail.com>
2023-10-10 21:22:13 -0300

    Colocando um poco de java

--- app/src/main/java/br/com/etecia/myapp/CalculoActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CalculoActivity.java
@@ -3,14 +3,23 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.EventListener;
 
 public class CalculoActivity extends AppCompatActivity {
 
-
+    TextInputLayout txtValor = findViewById(R.id.txtValor);
+    TextInputLayout txtQualidade = findViewById(R.id.txtQualidade);
+    Button btnCalcular = findViewById(R.id.btnCalcular);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo);
-
     }
+
+
+
 }
